4e32d31023bf7417878262211aa31a5cccaa6d17
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2024-02-27 10:13:30 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useLocation} from 'react-router-dom';
+
 
 function App() {
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+  const query = useQuery();
+  const searchQuery = query.get('userId'); // Получение значения параметра query
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{searchQuery}</h1>
     </div>
   );
 }
